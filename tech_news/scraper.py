1045a01d889b71acffac59a0edbe9ec64882273a
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-13 16:41:34 -0300

    feat:

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import requests
+from requests import HTTPError, ReadTimeout
+from time import sleep
+
+
 # Requisito 1
-def fetch(url):
-    pass
+def fetch(url, wait=3):
+    headers = {"user-agent": "Fake user-agent"}
+    try:
+        sleep(1)
+        response = requests.get(url, timeout=wait, headers=headers)
+        response.raise_for_status()
+    except(HTTPError, ReadTimeout):
+        return None
+
+    return response.text
 
 
 # Requisito 2
