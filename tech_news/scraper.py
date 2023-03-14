c7c8867287df856833c4d6cf9699ffc52fbf4222
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-13 21:16:57 -0300

    feat: Requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -20,17 +20,12 @@ def fetch(url, wait=3):
 # Requisito 2
 def scrape_updates(html_content):
     selector = Selector(html_content)
-
     try:
         quotes = selector.css('.entry-title > a::attr(href)').getall()
     except(requests.exceptions.ConnectionError):
         return []
 
     return quotes
-
-
-# html = fetch("https://blog.betrybe.com/")
-# print(scrape_updates(html))
 
 
 # Requisito 3
@@ -45,7 +40,27 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    scrape = {
+        "url": selector.css("link[rel='canonical']::attr(href)").get().strip(),
+        "title": selector.css('.entry-title::text').get().strip(),
+        "timestamp": selector.css('.meta-date::text').get().strip(),
+        "writer": selector.css('.author > a::text').get().strip(),
+        "reading_time": int(selector.css('.meta-reading-time::text')
+                            .get()[:2]),
+        "summary": "".join(
+            selector.css(".entry-content > p:first-of-type *::text").getall()
+        ).strip(),
+        "category": selector.css(".label::text").get().strip(),
+    }
+
+    return scrape
+
+
+# html = fetch("""https://blog.betrybe.com/linguagem-de-
+# programacao/o-que-e-array/""")
+# print(scrape_news(html))
 
 
 # Requisito 5
