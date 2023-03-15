7606b0de475e77c51a074db22f476741cffb57d2
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-14 21:11:12 -0300

    feat: Requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
+from tech_news.database import search_news
+
+
+def search_adapter(filter):
+    engine = search_news(filter)
+    news_list: list[tuple] = [(news["title"], news["url"]) for news in engine]
+
+    return news_list
+
+
 # Requisito 7
-def search_by_title(title):
-    """Seu código deve vir aqui"""
+def search_by_title(title: str):
+    news = search_adapter({"title": {"$regex": title, "$options": "i"}})
+    return news
 
 
 # Requisito 8
@@ -11,3 +22,6 @@ def search_by_date(date):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+# search_by_title("O que é array, para que serve e como fazer?")
