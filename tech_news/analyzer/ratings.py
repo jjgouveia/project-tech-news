268c242d6ca835d6e15e9c4e1bdafae67e624e3d
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-14 23:31:34 -0300

    feat: Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,15 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    query = [
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+
+    categories_list = get_collection().aggregate(query)
+    categories = [category["_id"] for category in categories_list]
+
+    return categories
